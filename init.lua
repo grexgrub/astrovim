aeded79627bf2676101e3c94ed405be40f4505cc
grexgrub <grexgrub@gmail.com>
2023-07-26 11:36:15 +0700

    asu

--- init.lua
+++ init.lua
@@ -64,22 +64,23 @@ return {
       },
     },
   },
+  
 
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
     -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
+     vim.filetype.add {
+       extension = {
+         ejs = "ejsscript",
+      },
+    --  filename = {
+    --    ["Ejsfile"] = "ejsscript",
+    --  },
+    --  pattern = {
+    --    ["~/%.config/foo/.*"] = "fooscript",
     --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
+    }
   end,
 }
